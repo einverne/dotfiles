30a87334ae31d98903bae0bbb6437052ee398e54
Ein Verne <einverne@gmail.com>
2017-12-29 14:09:37 +0800

    Install without promt and auto build xrectsel
    Signed-off-by: Ein Verne <einverne@gmail.com>

--- install_byzanz_record.sh
+++ install_byzanz_record.sh
@@ -1,16 +1,18 @@
 #!/bin/bash - 
 
 # take from http://askubuntu.com/questions/107726/how-to-create-animated-gif-images-of-a-screencast
-sudo apt-get install byzanz
+sudo apt-get install -y byzanz
 
 # example
 # byzanz-record --duration=15 --x=200 --y=300 --width=700 --height=400 out.gif
 
-git clone https://github.com/lolilolicon/xrectsel.git
 # install automake tool to avoid "autoreconf: not found" error
 # use the following command to install automake tool
-sudo apt-get install autoconf automake libtool build-essential checkinstall libx11-dev x11-utils
+sudo apt-get install -y autoconf automake libtool build-essential checkinstall libx11-dev x11-utils
 
+git clone https://github.com/lolilolicon/xrectsel.git && cd xrectsel 
+./bootstrap && ./configure --prefix /usr && make && sudo make install
+cd .. && rm -rf xrectsel
 
 # 1. byzanz-record-window - To select a window for recording.
 # 2. byzanz-record-region - To select a part of the screen for recording.
