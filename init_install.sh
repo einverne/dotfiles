fb9a8f3dfc2502ba7456eb94c5dfefe932796247
Ein Verne <einverne@gmail.com>
2019-01-09 11:36:41 +0800

    Add pyenv requirements

--- init_install.sh
+++ init_install.sh
@@ -27,3 +27,8 @@ chsh -s /bin/zsh
 # pyenv
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 
+# pyenv required
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+    xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+
