c5c7f2ab589e25cad081b6708c4f509a5d3e6c6d
Ein Verne <git@einverne.info>
2022-07-02 08:26:52 +0800

    Add zsh syntax highlighting

--- termux/setup.sh
+++ termux/setup.sh
@@ -2,7 +2,7 @@
 
 echo -e "Installing dependencies ..."
 # https://github.com/4679/oh-my-termux
-pkg install -y libcurl wget curl openssh vim git zsh unrar unzip less tree
+pkg install -y libcurl wget curl openssh vim git zsh unrar unzip less tree htop
 pkg install -y tsu neofetch
 echo -e "Successfully Installed"
 
@@ -21,6 +21,10 @@ curl -fsLo $HOME/.termux/font.ttf --create-dirs https://github.com/einverne/dotf
 git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh --depth 1
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' $HOME/.zshrc
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+
 chsh -s zsh
 
 termux-setup-storage
