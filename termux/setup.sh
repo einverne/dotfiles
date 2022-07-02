e9d3c00494c995834312eeb7191cc1582452e6e4
Ein Verne <git@einverne.info>
2022-07-02 08:19:36 +0800

    Update termux setup

--- termux/setup.sh
+++ termux/setup.sh
@@ -2,7 +2,7 @@
 
 echo -e "Installing dependencies ..."
 # https://github.com/4679/oh-my-termux
-pkg install -y libcurl wget curl openssh vim git zsh unrar unzip less
+pkg install -y libcurl wget curl openssh vim git zsh unrar unzip less tree
 pkg install -y tsu neofetch
 echo -e "Successfully Installed"
 
@@ -18,7 +18,7 @@ curl -fsLo $HOME/.termux/colors.properties --create-dirs https://github.com/einv
 curl -fsLo $HOME/.termux/termux.properties --create-dirs https://github.com/einverne/dotfiles/raw/master/termux/termux.properties
 curl -fsLo $HOME/.termux/font.ttf --create-dirs https://github.com/einverne/dotfiles/raw/master/termux/font.ttf
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
+git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh --depth 1
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' $HOME/.zshrc
 chsh -s zsh
